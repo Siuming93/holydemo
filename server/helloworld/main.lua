c242d8c0623dbfe10ec2ae1fe5e082d5f2a719a5
Siuming93 <2457944792@qq.com>
2017-07-13 00:01:32 +0800

    创建helloworld 服务

---
+++ server/helloworld/main.lua
@@ -0,0 +1,17 @@
+local skynet = require "skynet"
+
+local max_client = 64;
+
+skynet.start(function()
+	print("----server start----")
+
+	local watchdog = skynet.newserviece("watchdog")
+	skynet.call(watchdog, "lua", "start",{
+		port = 8888,
+		maxclient = max_client,
+		nodelay = true,
+		})
+	print("watchdog listen on 8888")
+
+	skynet.exit()	
+end)
