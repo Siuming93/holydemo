d6f1b6f316e6ffd31c403d2351b15cbd446293f8
Siuming93 <2457944792@qq.com>
2017-08-18 00:58:31 +0800

    新建聊天模块

---
+++ client/Trunk/Assets/Scripts/Modules/Chat/View/ChatPanelMediator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using XLua;
+
+public class ChatPanelMediator : AbstractMediator
+{
+    public new const string NAME = "ChatPanelMediator";
+    public const string PANEL_PATH = "";
+
+    private GameObject _view;
+    public ChatPanelMediator(): base(NAME)
+    {
+        RegisterNotifications();
+        CreateView();
+    }
+
+    [Hotfix]
+    private void RegisterNotifications()
+    {
+        
+    }
+
+    [Hotfix]
+    private void CreateView()
+    {
+        _view = Object.Instantiate(ResourcesFacade.Instance.Load<GameObject>(PANEL_PATH));
+        
+    }
+
+}
