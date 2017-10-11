ba559e3c544ddbd25fc6af970101aafc5165e5be
siuming <2457944792@qq.com>
2017-10-11 19:47:54 +0800

    增加meta模块

---
+++ client/Trunk/Assets/Scripts/BaseSystem/Meta/BaseMeta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public abstract class BaseMeta<T> where T : new()
+{
+    protected static Dictionary<string, T> map = new Dictionary<string, T>();
+    public static T GetMeta(string id)
+    {
+        T t;
+        map.TryGetValue(id, out t);
+        return t;
+    }
+
+    public string id;
+
+    public static void AddMeta(Hashtable properties)
+    {
+        string id = MetaUtil.GetStringValue(properties, "id");
+        if (!map.ContainsKey(id))
+        {
+            T t = new T();
+            map.Add(id, t);
+        }
+    }
+
+    public abstract void UpdateForm(Hashtable properties);
+    
+}
---
+++ client/Trunk/Assets/Scripts/BaseSystem/Meta/MetaUtil.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class MetaUtil
+{
+    public static string GetStringValue(Hashtable properties, string propertyName)
+    {
+        string val = properties[propertyName] as string;
+        return val;
+    }
+
+    public static int GetIntValue(Hashtable properties, string propertyName, int defaultVal = -1)
+    {
+        string val = properties[propertyName] as string;
+        int result = defaultVal;
+        if (val != null && int.TryParse(val, out result))
+        {
+            return result;
+        }
+        return defaultVal;
+    }
+
+    public static int GetIntValue(Hashtable properties, string propertyName, int minVal, int maxVal, int defaultVal)
+    {
+        int val = GetIntValue(properties, propertyName, defaultVal);
+        val = Mathf.Clamp(val, minVal, maxVal);
+        return val;
+    }
+
+    public static float GetFloatValue(Hashtable properties, string propertyName, float defaultVal = 0)
+    {
+        string val = properties[propertyName] as string;
+        float result = defaultVal;
+        if (val != null && float.TryParse(val, out result))
+        {
+            return result;
+        }
+        return defaultVal;
+    }
+}
