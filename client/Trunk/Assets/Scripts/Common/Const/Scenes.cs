e4a25f2e6d6f39fdf34b73cce9c6ee0a1183c56b
siuming <2457944792@qq.com>
2017-10-11 19:47:02 +0800

    增加常量;

---
+++ client/Trunk/Assets/Scripts/Common/Const/Scenes.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// 记录场景信息
+/// </summary>
+public class Scenes
+{
+    public const string loadLevel = "002-startvillage";
+}
---
+++ client/Trunk/Assets/Scripts/Common/Const/Tags.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 记录Tag配置信息
+/// </summary>
+public class Tags
+{
+    public const string MainCamera = "MainCamera";
+    public const string Player = "Player";
+    public const string Enemy = "Enemy";
+    public const string DamageSphere = "DamageSphere";
+}
