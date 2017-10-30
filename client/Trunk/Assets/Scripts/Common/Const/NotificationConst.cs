82fdeeb9f6630761943b69996911e7b0b41406da
siuming <2457944792@qq.com>
2017-10-30 18:15:49 +0800

    消息

--- client/Trunk/Assets/Scripts/Common/Const/NotificationConst.cs
+++ client/Trunk/Assets/Scripts/Common/Const/NotificationConst.cs
@@ -4,9 +4,11 @@
 
     public const string ON_GET_TALK_MSG = "OnGetTalkMsg";
 
-    public const string PLAYER_MOVE = "PlayerMove";
-    public const string PLAYER_MOVE_END = "PlayerMoveEnd";
-    public const string PLAYER_MOVE_START = "PlayerMoveStart";
+    public const string ALL_OTHER_PLAYER_INFO_UPDATE = "AllOtherPlayerInfoUpdate";
+    public const string PLAYER_INFO_UPDATE = "PlayerInfoUpdate";
+    public const string PLAYER_START_MOVE = "PlayerStartMove";
+    public const string PLAYER_END_MOVE = "PlayerEndMove";
+    public const string PLAYER_UPDATE_MOVE_DIR = "PlayerUpdateMoveDir";
 
     public const string USE_SKILL = "UseSkill";
 }
