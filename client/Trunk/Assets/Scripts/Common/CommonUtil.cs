9ef2bba44e8a9dcfd4d3d78121c003726c0f7f3b
Siuming93 <2457944792@qq.com>
2017-11-22 23:53:59 +0800

    增加重启功能;增加时间同步Proxy;

---
+++ client/Trunk/Assets/Scripts/Common/CommonUtil.cs
@@ -0,0 +1,12 @@
+﻿
+using Monster.BaseSystem;
+using Monster.BaseSystem.SceneManager;
+
+public class CommonUtil
+{
+    public static void ReStart()
+    {
+        UpdateProxy.Instance.StartCoroutine(SceneSwitcher.Instance.LoadScene(PreloadSceneManager.SCENE_NAME));
+    }
+}
+
---
+++ client/Trunk/Assets/Scripts/Common/TimeProxy.cs
@@ -0,0 +1,27 @@
+﻿
+public class TimeProxy : BaseProxy
+{
+    public new const string NAME = "TimeProxy";
+
+    /// <summary>
+    /// Server Millisecond
+    /// </summary>
+    public static int Time
+    {
+        get { return System.DateTime.Now.Millisecond - _lastLocaleTime + _lastServerMillisecond; }
+    }
+    public TimeProxy(): base(NAME)
+    {
+        OnAsyncServerTime(null);
+    }
+
+    private static int _lastLocaleTime;
+    private static int _lastServerMillisecond;
+
+    private void OnAsyncServerTime(object data)
+    {
+        _lastLocaleTime = System.DateTime.Now.Millisecond;
+        _lastServerMillisecond = _lastLocaleTime;
+    }
+}
+
