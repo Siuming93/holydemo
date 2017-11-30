3258e98354e9d7d263719fa484cb2908cd263ee4
Siuming93 <2457944792@qq.com>
2017-12-01 00:11:22 +0800

    增加timeproxy;

---
+++ client/Trunk/Assets/Scripts/Common/Time/TimeProxy.cs
@@ -0,0 +1,39 @@
+﻿
+using Monster.BaseSystem;
+using Monster.Protocol;
+
+public class TimeProxy : BaseProxy
+{
+    public new const string NAME = "TimeProxy";
+
+
+    public TimeProxy(): base(NAME)
+    {
+        RegisterMessageHandler(MsgIDDefine.ScAsyncTime, OnAsyncServerTime);
+        netManager.SendMessage(new CsAsyncTime() { id = (int)PlayerProperty.ID });   
+        UpdateProxy.Instance.UpdateEvent += AsyncTime;
+    }
+
+    public override void OnRemove()
+    {
+        UpdateProxy.Instance.UpdateEvent -= AsyncTime;
+        base.OnRemove();
+    }
+
+
+    private void OnAsyncServerTime(object data)
+    {
+        ScAsyncTime msg = data as ScAsyncTime;
+        GameConfig.lastServerTime = msg.time;
+    }
+
+    private void AsyncTime()
+    {
+        if (UnityEngine.Time.time - GameConfig.lastLocaleTime >= 10)
+        {
+            netManager.SendMessage(new CsAsyncTime(){id = (int)PlayerProperty.ID});
+            GameConfig.lastLocaleTime = UnityEngine.Time.time; 
+        }
+    }
+}
+
---
+++ client/Trunk/Assets/Scripts/Common/Time/TimeUtil.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Diagnostics;
+
+public enum TimeStyle
+{
+    HHMMSS =1,
+    YYMMDD =2,
+}
+public static class TimeUtil
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public static string ToLongTimeString(double seconds)
+    {
+        DateTime now = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Unspecified).AddMilliseconds(seconds * 1000).ToLocalTime();
+        return now.ToLongTimeString();
+        //return string.Format("{0:yy-MM-dd hh:mm:ss zz}", now);
+    }
+
+    public static string ToShortTimeString(double seconds)
+    {
+        DateTime now = new DateTime(1970, 1, 1).AddMilliseconds(seconds * 1000);
+        return now.ToShortTimeString();
+    }
+
+    public static string ToTimeString(double seconds, TimeStyle style = TimeStyle.HHMMSS)
+    {
+        return "";
+    }
+}
+
