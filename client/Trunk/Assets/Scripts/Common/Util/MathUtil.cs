1d568c0816cc75e86769fd5efd07f2f0c8995c95
siuming <2457944792@qq.com>
2017-12-01 21:27:52 +0800

    计算方向;

---
+++ client/Trunk/Assets/Scripts/Common/Util/MathUtil.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public static class MathUtil
+{
+    public static Vector2 GetCoordinate(float angle)
+    {
+        //Debug.Log("angle:" + angle + " dir:" + new Vector2(Mathf.Cos(angle * Mathf.PI / 180f), Mathf.Sin(angle * Mathf.PI / 180f)));
+        return new Vector2(Mathf.Cos(angle * Mathf.PI / 180f), Mathf.Sin(angle * Mathf.PI / 180f));
+    }
+}
+
