7aeb9abdeab83238a686c2d777057ee37d422405
siuming <2457944792@qq.com>
2018-01-09 12:07:18 +0800

    添加热更工程;

---
+++ client/HotFix/GameLogic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace HotFix
+{
+    public class GameLogic
+    {
+        public static float _lastLogTime;
+        public static int _count;
+        public static void Tick()
+        {
+            if (Time.time - _lastLogTime >= 1)
+            {
+                UnityEngine.Debug.Log(_count++);
+                _lastLogTime = Time.time;
+            }
+        }
+    }
+}
