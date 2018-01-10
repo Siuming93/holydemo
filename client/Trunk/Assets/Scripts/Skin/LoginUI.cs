97dd5d4a8312b62970c46536a29f07ba19de572e
siuming <2457944792@qq.com>
2018-01-10 14:21:50 +0800

    移动LoginUI;

---
+++ client/Trunk/Assets/Scripts/Skin/LoginUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Monster.Net;
+public class LoginUI : MonoBehaviour
+{
+    public InputField acountInputField;
+    public InputField ipAddressInputField;
+    public Button loginButton;
+    public Text stateText;
+}
